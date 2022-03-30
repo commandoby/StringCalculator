87c0a24d5102108495bd33fcb67383504a02c286
commandoby <beresten20117@mail.ru>
2022-03-30 23:20:16 +0300

    v0.01.00011

--- src/main/java/com/commandoby/stringCalculator/Application.java
+++ src/main/java/com/commandoby/stringCalculator/Application.java
@@ -22,51 +22,56 @@ import com.commandoby.stringCalculator.swing.impl.ViewConsoleSwingImpl;
 class Application {
 	private static final String HELP = "exit - complete the program;\n" + "help - help for valid commands;\n"
 			+ "point - numbers after the decimal point (Default: 2);\n" + "more on/more off - Detailed solution.\n"
-			+ "Example: 1.5 * (2 - 3) + 4^0.5";
+			+ "Example: 1.5 * (2 - 3) + 4^0.5\n";
 
 	private static Reader reader = new ReaderImpl();
 	private static Solver solver = new SolverImpl();
 	private static Writer writer = new WriterImpl();
 	private static Logger log = Logger.getLogger(Application.class);
 
+	static Scanner scanner = new Scanner(System.in);
+	static boolean active = true;
+
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		String text;
-		boolean active = true;
 
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new ViewConsoleSwingImpl();
+		if (args.length > 0 && args[0].equalsIgnoreCase("console")) {
+			while (active) {
+				System.out.print("Enter the equation: ");
+				text = scanner.nextLine().trim();
+
+				System.out.println(textAnalysis(text));
 			}
-		});
+		} else {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					new ViewConsoleSwingImpl();
+				}
+			});
+		}
+	}
 
-		while (active) {
-			System.out.print("Enter the equation: ");
-			text = scanner.nextLine().trim();
-
-			switch (text) {
-			case "help":
-				System.out.println(HELP);
-				break;
-			case "exit":
-				active = false;
-				scanner.close();
-				break;
-			case "point":
-				System.out.print("Enter the number of digits after the decimal point: ");
-				WriterImpl.numbersAfterTheDecimalPoint = Integer.parseInt(scanner.nextLine().trim());
-				break;
-			case "more on":
-				System.out.println("Detailed solution included.");
-				SolverImpl.detailedSolution = true;
-				break;
-			case "more off":
-				System.out.println("Detailed solution is off.");
-				SolverImpl.detailedSolution = false;
-				break;
-			default:
-				getAnswer(text);
-			}
+	public static String textAnalysis(String text) {
+		switch (text) {
+		case "help":
+			return HELP;
+		case "exit":
+			active = false;
+			scanner.close();
+			return "";
+		case "point":
+			System.out.print("Enter the number of digits after the decimal point: ");
+			WriterImpl.numbersAfterTheDecimalPoint = Integer.parseInt(scanner.nextLine().trim());
+			return "The number of decimal places is set to " + WriterImpl.numbersAfterTheDecimalPoint + ".\n";
+		case "more on":
+			SolverImpl.detailedSolution = true;
+			return "Detailed solution included.\n";
+		case "more off":
+			SolverImpl.detailedSolution = false;
+			return "Detailed solution is off.\n";
+		default:
+			getAnswer(text);
+			return "";
 		}
 	}
 
