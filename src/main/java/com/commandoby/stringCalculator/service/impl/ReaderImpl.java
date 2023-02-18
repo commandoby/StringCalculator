d91f8591b6ddf156ff559793e37bc2623711a409
commandoby <beresten20117@mail.ru>
2023-02-18 20:20:04 +0300

    v1.03.00039 change read number metods

--- src/main/java/com/commandoby/stringCalculator/service/impl/ReaderImpl.java
+++ src/main/java/com/commandoby/stringCalculator/service/impl/ReaderImpl.java
@@ -24,64 +24,74 @@ public class ReaderImpl implements Reader {
 	private Operand inclusiveOperand = new Operand(null, 0);
 	private String currentText;
 
+	private static List<Operation> firstOperationList;
+	private static List<Operation> secondOperationList;
+	private static List<Operation> lastOperationList;
 	private static Pattern splitOfSubEquationsPattern;
+	private static Pattern splitOfSubEquationsEndPattern;
 	private static Pattern splitOfNumbersPattern;
-	private static Pattern checkNegativeNumberPattern;
 
 	static {
-		List<String> firstOperationStringList = Stream.of(Operation.values())
-				.filter(t -> t.getType() == OperationType.FIRST).map(Operation::getPattern)
+		firstOperationList = Stream.of(Operation.values()).filter(t -> t.getType() == OperationType.FIRST)
 				.collect(Collectors.toList());
-		List<String> secondOperationStringList = Stream.of(Operation.values())
-				.filter(t -> t.getType() == OperationType.SECOND).map(Operation::getPattern)
+		secondOperationList = Stream.of(Operation.values()).filter(t -> t.getType() == OperationType.SECOND)
 				.collect(Collectors.toList());
-		List<String> lastOperationStringList = Stream.of(Operation.values())
-				.filter(t -> t.getType() == OperationType.LAST).map(Operation::getPattern).collect(Collectors.toList());
+		lastOperationList = Stream.of(Operation.values()).filter(t -> t.getType() == OperationType.LAST)
+				.collect(Collectors.toList());
 
 		StringBuilder firstBuilder = new StringBuilder();
-		for (String s : firstOperationStringList) {
+		for (Operation s : firstOperationList) {
 			if (!firstBuilder.isEmpty()) {
 				firstBuilder.append("|");
 			}
-			firstBuilder.append(s);
+			firstBuilder.append(s.getPattern());
 		}
 		StringBuilder secondBuilder = new StringBuilder();
-		for (String s : secondOperationStringList) {
+		for (Operation s : secondOperationList) {
 			if (!secondBuilder.isEmpty()) {
 				secondBuilder.append("|");
 			}
-			secondBuilder.append(s);
+			secondBuilder.append(s.getPattern());
 		}
 		StringBuilder lastBuilder = new StringBuilder();
-		for (String s : lastOperationStringList) {
+		for (Operation s : lastOperationList) {
 			if (!lastBuilder.isEmpty()) {
 				lastBuilder.append("|");
 			}
-			lastBuilder.append(s);
+			lastBuilder.append(s.getPattern());
 		}
 
-		splitOfSubEquationsPattern = Pattern.compile("(" + firstBuilder.toString() + ")*\\s*\\(");
-		splitOfNumbersPattern = Pattern.compile("(" + firstBuilder.toString() + ")*\\s*(" + secondBuilder.toString()
-				+ ")\\s*\\d+(\\.|,)?\\d*\\s*(" + lastBuilder.toString() + ")");
-		//checkNegativeNumberPattern = Pattern.compile("(" + firstBuilder.toString() + "|)+\\s*-\\s*\\d");
+		splitOfSubEquationsPattern = Pattern
+				.compile("(" + firstBuilder.toString() + ")?\\s*(" + secondBuilder.toString() + ")?\\s*\\(");
+		splitOfSubEquationsEndPattern = Pattern
+				.compile("\\)(" + lastBuilder.toString() + ")?");
+		splitOfNumbersPattern = Pattern.compile("(" + firstBuilder.toString() + ")?\\s*(" + secondBuilder.toString()
+				+ ")?\\s*\\d+(\\.|,)?\\d*\\s*(" + lastBuilder.toString() + ")?");
 	}
 
 	@Override
 	public Operand read(String text) throws InvalidCharacterException, SubEquationException {
 		currentText = text;
 		Operand operand = new Operand(null, 0);
+		// Operand inclusiveOperand = new Operand(null, 0);
 		List<String> textOperands = split();
 
+		System.out.println(textOperands);
 		for (String s : textOperands) {
-			 if (s.matches(".*\\(+.*")) {
-			// inclusiveOperand = read(s.substring(0, s.length() -
-			// 1).replaceFirst("[^0-9\\s]*\\s*\\(", ""));
-			// readOperation(s, "\\s*\\-?\\s*\\(");
-			 } else {
-			// readNumber(s);
-			// readOperation(s, "\\s*\\-?\\s*\\d");
-				 readFirstOperation(s);
-			 }
+			if (s.matches(".*\\(+.*")) {
+				// inclusiveOperand = read(s.substring(0, s.length() -
+				// 1).replaceFirst("[^0-9\\s]*\\s*\\(", ""));
+				// readOperation(s, "\\s*\\-?\\s*\\(");
+			} else {
+				s = readFirstOperation(s);
+				s = readSecondAndLastOperation(s, secondOperationList);
+				if (inclusiveOperand.size() == 0) {
+					s = readSecondAndLastOperation(s, lastOperationList);
+					if (inclusiveOperand.size() == 0) {
+						readNumber(s);
+					}
+				}
+			}
 			operand.add(inclusiveOperand);
 			inclusiveOperand = new Operand(null, 0);
 		}
@@ -92,7 +102,7 @@ public class ReaderImpl implements Reader {
 			operand.get(0).setOperation(SECOND_SUBTRACT);
 		}
 
-		//System.out.println(operand);
+		System.out.println(operand);
 		return operand;
 	}
 
@@ -141,11 +151,17 @@ public class ReaderImpl implements Reader {
 			int countOfClosingIndex = 0;
 			for (int i = 0; i < countOfClosing.size(); i++) {
 				if (i == countOfClosing.size() - 1 || countOfClosing.get(i) < countOfOpening.get(i + 1)) {
-					countOfClosingIndex = i;
+					countOfClosingIndex = countOfClosing.get(i);
 					break;
 				}
 			}
-			String subText = currentText.substring(matcherOfStart.start(), countOfClosing.get(countOfClosingIndex) + 1);
+			
+			Matcher endMatcher = splitOfSubEquationsEndPattern.matcher(currentText);
+			if (endMatcher.find(countOfClosingIndex-1)) {
+				countOfClosingIndex++;
+			}
+			
+			String subText = currentText.substring(matcherOfStart.start(), countOfClosingIndex + 1);
 			map.put(matcherOfStart.start(), subText);
 
 			StringBuilder sb = new StringBuilder();
@@ -165,25 +181,45 @@ public class ReaderImpl implements Reader {
 		}
 	}
 
-	private void readFirstOperation(String text) throws InvalidCharacterException {
-		for (Operation operation : Operation.values()) {
-			Matcher symbolMatcher = Pattern.compile(Pattern.quote(operation.getPattern())).matcher(text);
+	private String readFirstOperation(String text) throws InvalidCharacterException {
+		for (Operation operation : firstOperationList) {
+			Matcher symbolMatcher = Pattern.compile(operation.getPattern()).matcher(text);
 			if (symbolMatcher.find()) {
 				inclusiveOperand.setOperation(operation);
-				text = text.replaceFirst(operation.getText().trim(), "");
-				return;
+				text = text.replaceFirst(operation.getPattern(), "");
+				return text;
 			}
 		}
 
-		Matcher matcher = Pattern.compile(Pattern.quote("\\s*[^0-9\\(\\s]*\\s*-?\\s*(\\d|\\()")).matcher(text);
-		if (matcher.find()) {
-			throw new InvalidCharacterException("Invalid character: " + matcher.group());
+		// Matcher matcher =
+		// Pattern.compile(Pattern.quote("\\s*[^0-9\\(\\s]*\\s*-?\\s*(\\d|\\()")).matcher(text);
+		// if (matcher.find()) {
+		// throw new InvalidCharacterException("Invalid character: " + matcher.group());
+		// }
+		return text;
+	}
+
+	private String readSecondAndLastOperation(String text, List<Operation> operations)
+			throws InvalidCharacterException, SubEquationException {
+		for (Operation operation : operations) {
+			Matcher symbolMatcher = Pattern.compile(operation.getPattern()).matcher(text);
+			if (symbolMatcher.find()) {
+				if (inclusiveOperand.getOperation() == null) {
+					inclusiveOperand.setOperation(operation);
+					text = text.replaceFirst(operation.getPattern(), "");
+					return text;
+				} else {
+					Reader reader = new ReaderImpl();
+					inclusiveOperand.addAll(reader.read(text));
+					return text;
+				}
+			}
 		}
+		return text;
 	}
 
 	private void readNumber(String text) {
-		Pattern pattern = Pattern.compile("\\d+(\\.|,)?\\d*");
-		Matcher matcher = pattern.matcher(text);
+		Matcher matcher = Pattern.compile("\\d+(\\.|,)?\\d*").matcher(text);
 		if (matcher.find()) {
 			String numberString = matcher.group();
 
@@ -192,16 +228,7 @@ public class ReaderImpl implements Reader {
 			String numberStringWithoutDecimalPoint = decimalPointMatcher.replaceAll(".");
 
 			double value = Double.valueOf(numberStringWithoutDecimalPoint);
-			//inclusiveOperand.setOperandNumber(checkNegativeNumber(value, text));
 			inclusiveOperand.setOperandNumber(value);
 		}
-	}
-
-	private double checkNegativeNumber(double value, String text) {
-		Matcher matcher = checkNegativeNumberPattern.matcher(text);
-		if (matcher.find()) {
-			value *= -1;
-		}
-		return value;
 	}
 }
