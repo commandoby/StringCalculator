4e4118fbddd628765c5038a6c3ca8dedd9efd08e
commandoby <beresten20117@mail.ru>
2023-02-20 22:42:23 +0300

    v1.03.00041 change writeOperandNumber method

--- src/main/java/com/commandoby/stringCalculator/service/impl/WriterImpl.java
+++ src/main/java/com/commandoby/stringCalculator/service/impl/WriterImpl.java
@@ -1,33 +1,35 @@
 package com.commandoby.stringCalculator.service.impl;
 
+import java.math.BigDecimal;
+
 import com.commandoby.stringCalculator.components.Operand;
 import com.commandoby.stringCalculator.enums.OperationType;
 import com.commandoby.stringCalculator.exceptions.WriteException;
 import com.commandoby.stringCalculator.service.Writer;
 
 public class WriterImpl implements Writer {
-	public static int numbersAfterTheDecimalPoint = 2;
+	public static int numbersAfterTheDecimalPoint = 5;
 
 	@Override
 	public String write(Operand operand) throws WriteException {
 		StringBuilder sb = new StringBuilder();
 
-		for (Operand subOperand: operand) {
+		for (Operand subOperand : operand) {
 
-			if (subOperand.getOperation() != null && subOperand.getOperation().getType()!=OperationType.LAST) {
+			if (subOperand.getOperation() != null && subOperand.getOperation().getType() != OperationType.LAST) {
 				sb.append(subOperand.getOperation().getText());
 			}
 			if (subOperand.isEmpty()) {
 				sb.append(writeOperandNumber(subOperand.getOperandNumber()));
 			} else {
 				Writer writer = new WriterImpl();
-				if (subOperand.size()>1) {
-				sb.append("(" + writer.write(subOperand) + ")");
+				if (subOperand.size() > 1) {
+					sb.append("(" + writer.write(subOperand) + ")");
 				} else {
 					sb.append(writer.write(subOperand));
 				}
 			}
-			if (subOperand.getOperation() != null && subOperand.getOperation().getType()==OperationType.LAST) {
+			if (subOperand.getOperation() != null && subOperand.getOperation().getType() == OperationType.LAST) {
 				sb.append(subOperand.getOperation().getText());
 			}
 		}
@@ -36,9 +38,14 @@ public class WriterImpl implements Writer {
 
 	@Override
 	public String writeOperandNumber(double number) {
-		if (number % 1 == 0) {
-			return String.format("%1.0f", number);
+		BigDecimal bigNumber = new BigDecimal(number);
+		int i = 0;
+
+		while (bigNumber.remainder(new BigDecimal(1).divide(new BigDecimal(10).pow(i))).compareTo(BigDecimal.ZERO) != 0
+				&& i < numbersAfterTheDecimalPoint) {
+			i++;
 		}
-		return String.format("%1." + numbersAfterTheDecimalPoint + "f", number);
+
+		return String.format("%1." + i + "f", number);
 	}
 }
