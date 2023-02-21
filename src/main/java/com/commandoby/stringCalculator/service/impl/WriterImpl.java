14fbc1fbda5a0a0ba2c69e66d72c24ca112f2bbb
commandoby <beresten20117@mail.ru>
2023-02-21 21:28:56 +0300

    v1.03.00042 added decimalList

--- src/main/java/com/commandoby/stringCalculator/service/impl/WriterImpl.java
+++ src/main/java/com/commandoby/stringCalculator/service/impl/WriterImpl.java
@@ -1,6 +1,8 @@
 package com.commandoby.stringCalculator.service.impl;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.commandoby.stringCalculator.components.Operand;
 import com.commandoby.stringCalculator.enums.OperationType;
@@ -9,10 +11,13 @@ import com.commandoby.stringCalculator.service.Writer;
 
 public class WriterImpl implements Writer {
 	public static int numbersAfterTheDecimalPoint = 5;
+	private static List<BigDecimal> decimalList = new ArrayList<>();
 
 	@Override
 	public String write(Operand operand) throws WriteException {
 		StringBuilder sb = new StringBuilder();
+
+		updateDecimalList();
 
 		for (Operand subOperand : operand) {
 
@@ -41,11 +46,17 @@ public class WriterImpl implements Writer {
 		BigDecimal bigNumber = new BigDecimal(number);
 		int i = 0;
 
-		while (bigNumber.remainder(new BigDecimal(1).divide(new BigDecimal(10).pow(i))).compareTo(BigDecimal.ZERO) != 0
-				&& i < numbersAfterTheDecimalPoint) {
+		while (i < numbersAfterTheDecimalPoint
+				&& bigNumber.remainder(decimalList.get(i)).compareTo(BigDecimal.ZERO) != 0) {
 			i++;
 		}
 
 		return String.format("%1." + i + "f", number);
+	}
+
+	private static void updateDecimalList() {
+		while (decimalList.size() < numbersAfterTheDecimalPoint) {
+			decimalList.add(new BigDecimal(1).divide(new BigDecimal(10).pow(decimalList.size())));
+		}
 	}
 }
